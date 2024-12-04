48c1c84ab39c0fe318deab8213351c0d895bd692
Eladiosf <elasf65@gmail.com>
2024-12-04 10:11:09 +0100

    Creación tabla cabecera de las órdenes

---
+++ app/Models/Order.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Order extends Model
+{
+    protected $fillable = [
+        'user_id',
+        'grand_total',
+        'payment_method',
+        'payment_status',
+        'status',
+        'currency',
+        'shipping_amount',
+        'shipping_method',
+        'notes',
+    ];
+
+    public function user():BelongsTo {
+        return $this->belongsTo(User::class);
+    }
+}
