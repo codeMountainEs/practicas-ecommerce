a0f9b2c26644507a5bc2ce009cb5cf5630ffff4e
Eladiosf <elasf65@gmail.com>
2024-12-04 11:01:47 +0100

    Creación tabla Direcciones de envío y borrado controlador de Ordenes

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Order extends Model
 {
@@ -21,5 +22,9 @@ class Order extends Model
 
     public function user():BelongsTo {
         return $this->belongsTo(User::class);
+    }
+
+    public function address():HasOne {
+        return $this->hasOne(Address::class);
     }
 }
