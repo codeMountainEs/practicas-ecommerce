9edc81dc56eb8b90d175e98dcc838ef9d0f6c8c0
codeMountainEs <codemountain.es@icloud.com>
2024-12-17 10:44:51 +0100

    Front Imagenes

--- resources/views/livewire/home-page.blade.php
+++ resources/views/livewire/home-page.blade.php
@@ -1,4 +1,5 @@
 <div>
+    @livewire('inicio.imagenes', ['categories' =>$categories])
 
 
 {{-- CATEGORY SECTION --}}
@@ -29,7 +30,29 @@
     <div class="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3 sm:gap-6">
 
         {{-- Brands--}}
-              {{-- fin Category--}}
+        @foreach($categories as $category)
+            <a class="group flex flex-col bg-white border shadow-sm rounded-xl hover:shadow-md transition dark:bg-slate-900 dark:border-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" href="/products)selected_categories[0]={{ $category->id }}" wire:key="{{ $category->id }}" >
+                <div class="p-4 md:p-5">
+                    <div class="flex justify-between items-center">
+                        <div class="flex items-center">
+                            <img class="h-[2.375rem] w-[2.375rem] rounded-full" src="{{ asset('storage/'. $category->image) }}" alt="{{ $category->name }}"  alt="Image Description">
+                            <div class="ms-3">
+                                <h3 class="group-hover:text-blue-600 font-semibold text-gray-800 dark:group-hover:text-gray-400 dark:text-gray-200">
+                                    {{ $category->name }}
+                                </h3>
+                            </div>
+                        </div>
+                        <div class="ps-3">
+                            <svg class="flex-shrink-0 w-5 h-5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                                <path d="m9 18 6-6-6-6" />
+                            </svg>
+                        </div>
+                    </div>
+                </div>
+            </a>
+
+        @endforeach
+        {{-- fin Category--}}
 
 
 
