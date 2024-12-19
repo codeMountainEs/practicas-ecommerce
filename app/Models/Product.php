fb8bb320bb32aeafe8c5500eeec9784ab5ca0f43
bbrahim98 <brahim98.taza98@gmail.com>
2024-12-19 13:05:59 +0100

    Creo un trait para gestionar las rutas amigables del slug, y hago todo el documento compatible entre sí en la parte que respecta a productos. Además, modufico product-resource para hacerlo compatible con el modelo de product

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -3,16 +3,18 @@
 namespace App\Models;
 
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Traits\HasSlug;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Product extends Model
 {
     /** @use HasFactory<\Database\Factories\ProductFactory> */
     use HasFactory;
+    use HasSlug;
 
-    protected $fillable = ['category_id','brand_id','name','slug','images','descirption','price','is_active','is_featured','in_stock','on_sale'];
+    protected $fillable = ['category_id','brand_id','name','slug','images','description','price','is_active','is_featured','in_stock','on_sale'];
 
     protected $casts = ['images' => 'array'];
 
